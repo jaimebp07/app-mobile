fc92909490c3f4078eb3b2cfa03a7770509de087
jaimebp07 <andrescodigo07@gmail.com>
2025-03-26 23:33:44 -0500

    🎉 Set up the first web component of the portealbo-wc library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import "portealbo-wc";
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app-mobile';
+  title = 'portealbo-app';
 }
