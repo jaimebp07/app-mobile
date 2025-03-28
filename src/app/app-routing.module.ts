4508197aa02e2022148339276ff5bf11a5c3cad4
jaimebp07 <andrescodigo07@gmail.com>
2025-03-27 21:08:31 -0500

    🔧 set up routing in the project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { LoginComponent } from './core/auth/login/login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: 'login', component: LoginComponent}
+]; 
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import "portealbo-wc";
-
 
 @Component({
   selector: 'app-root',
